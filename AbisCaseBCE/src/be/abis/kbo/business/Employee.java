5709572cc2b044f1e4f4be0529814304314a4de1
Sophie Roch <scallakim@hotmail.com>
2019-08-09 16:12:28 +0200

    Create Employee

---
+++ AbisCaseBCE/src/be/abis/kbo/business/Employee.java
@@ -0,0 +1,37 @@
+package be.abis.kbo.business;
+
+import java.security.*;
+
+import javax.xml.bind.DatatypeConverter;
+
+public class Employee {
+	private String login;
+	private String password;
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = hashMD5(password);
+	}
+	
+	private String hashMD5(String text) {
+		MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        md.update(text.getBytes());
+        byte[] digest = md.digest();
+        String myHash = DatatypeConverter.printHexBinary(digest).toUpperCase();
+
+        return myHash.toLowerCase();
+	}
+}

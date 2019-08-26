4a3aa90f570ca4c03fdd75c89f029c96b37fee20
Abdelilah <abdelilah.razzouk@student.ehb.be>
2019-08-26 11:06:33 +0200

    CLass worker Created

--- src/be/abis/casebce/model/Worker.java
+++ src/be/abis/casebce/model/Worker.java
@@ -1,6 +1,80 @@
 package be.abis.casebce.model;
 
-public class Worker {
-	//test
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
 
+@Named
+@SessionScoped
+public class Worker {
+	private String login;
+	private String password;
+	private String lastName;
+	private String firstName;
+	private String bankAcount;
+	@Inject
+	private Address address;
+	
+	public Worker(String login, String password, String lastName, String firstName, String bankAcount,
+			Address address) {
+		this.login = login;
+		this.password = password;
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.bankAcount = bankAcount;
+		this.address = address;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getBankAcount() {
+		return bankAcount;
+	}
+
+	public void setBankAcount(String bankAcount) {
+		this.bankAcount = bankAcount;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	
+	
+
+	
 }

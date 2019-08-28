1d5792582b2781f1163518aa078a99fab58558ae
Sophie Roch <scallakim@hotmail.com>
2019-08-28 13:37:01 +0200

    Activity Potential projects management

--- src/be/abis/casebce/controller/ActivityController.java
+++ src/be/abis/casebce/controller/ActivityController.java
@@ -1,6 +1,7 @@
 package be.abis.casebce.controller;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
@@ -8,6 +9,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import be.abis.casebce.model.Activity;
+import be.abis.casebce.model.Project;
 
 @Named
 @SessionScoped
@@ -17,6 +19,7 @@ public class ActivityController implements Serializable {
 	@Inject
 	private Activity currentActivity;
 	private List<Activity> displayedActivities;
+	private List<Project> potentialProjects;
 
 	public Activity getCurrentActivity() {
 		return currentActivity;
@@ -32,5 +35,20 @@ public class ActivityController implements Serializable {
 
 	public void setDisplayedActivities(List<Activity> displayedActivities) {
 		this.displayedActivities = displayedActivities;
+	}
+
+	public List<Project> getPotentialProjects() {
+		return potentialProjects;
+	}
+
+	public void setPotentialProjects(List<Project> potentialProjects) {
+		this.potentialProjects = potentialProjects;
+	}
+
+	public String validateEdition() {
+		this.currentActivity.setEnd(LocalDateTime.of(this.currentActivity.getStart().getYear(),
+				this.currentActivity.getStart().getMonth(), this.currentActivity.getStart().getDayOfMonth(),
+				this.currentActivity.getEnd().getHour(), this.currentActivity.getEnd().getMinute()));
+		return "activityInfo.xhtml?faces-redirected=true";
 	}
 }

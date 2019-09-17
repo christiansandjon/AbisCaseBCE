4e3c5cab1832a05b7096f4e1a30633b1e005d219
Sophie Roch <scallakim@hotmail.com>
2019-09-17 14:50:48 +0200

    Login Redirection

--- src/be/abis/casebce/controller/LoginController.java
+++ src/be/abis/casebce/controller/LoginController.java
@@ -3,6 +3,7 @@ package be.abis.casebce.controller;
 import java.io.Serializable;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.ConfigurableNavigationHandler;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
@@ -20,35 +21,48 @@ public class LoginController implements Serializable {
 	@Inject
 	@Named("worker")
 	private Worker worker;
-	
+	private boolean loginIsOk = false;
+
 	private WorkerService service = new WorkerService();
-	
+
 	public Login getLogin() {
 		return login;
 	}
+
 	public void setLogin(Login login) {
 		this.login = login;
 	}
+
 	public Worker getWorker() {
 		return worker;
 	}
+
 	public void setWorker(Worker worker) {
 		this.worker = worker;
 	}
-	
+
 	public String login() {
 		try {
 			this.worker = this.service.login(this.login);
 		} catch (Exception e) {
 			System.out.println(e.toString());
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Login Issue", e.getMessage()));
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Login Issue", e.getMessage()));
 			return "login";
 		}
+		this.loginIsOk = true;
 		return "activitydisplay?faces-redirect=true";
 	}
-	
+
 	public String logout() {
 		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
 		return "login?faces-redirect=true";
+	}
+
+	public void checkLogin() {
+		if (!this.loginIsOk) {
+			ConfigurableNavigationHandler handler = (ConfigurableNavigationHandler) FacesContext.getCurrentInstance().getApplication().getNavigationHandler();
+			handler.performNavigation("login?faces-redirect=true");
+		}
 	}
 }
